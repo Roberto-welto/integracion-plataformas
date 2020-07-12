21dcdf7ddfbd7d7c498b8fd7d3a2b15ea56993cf
Roberto-welto <rgonzalez@arkho.tech>
2020-07-12 17:51:28 -0400

    added changes

--- sistema_adopcion/sistema_adopcion/adopcion_backend/urls.py
+++ sistema_adopcion/sistema_adopcion/adopcion_backend/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls import url
 from django.urls import path, include
 
 from rest_framework import routers
-from .views import user_list, user_detail, pet_list, pet_detail, user_pets, get_petbd, get_doc, get_tipo_mascota
+from .views import user_list, user_detail, pet_list, pet_detail, user_pets, get_petbd, get_doc, get_tipo_mascota, comunas, donate
 
 urlpatterns = [
     path('user', user_list),
     path('user/<int:pk>', user_detail),
     path('pet', get_petbd),
     path('tipo_mascota', get_tipo_mascota),
+    path('comuna', comunas),
     path('doc', get_doc),
     path('user_pets/<int:idUser>', user_pets),
+    path('donar', donate)
 ]
--- sistema_adopcion/sistema_adopcion/adopcion_backend/views.py
+++ sistema_adopcion/sistema_adopcion/adopcion_backend/views.py
@@ -5,7 +5,12 @@ from django.views.generic import TemplateView
 from io import BytesIO
 import json
 import boto3
+import jwt
+import datetime
+import os
 # Create your views here.
+import stripe
+#
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -183,20 +188,122 @@ def get_doc(req):
   except Exception as e:
     print(str(e))
 
+@api_view(["GET"])
+def comunas(req):
+  
+  if req.method == 'GET':
+
+      connection = psycopg2.connect(
+        user='postgres',
+        password="1303",
+        host="localhost",
+        port="5432",
+        database = "sistema_adopcion")
+
+      cursor = connection.cursor()
+
+      cursor.execute(f"SELECT * FROM sistema_adopcion.f_obtener_comunas(next_set := CAST ({50} AS INTEGER), off  := CAST ({0} AS INTEGER), sort := CAST('{'DESC'}' AS VARCHAR));")
+
+      comunas = cursor.fetchone()
+      comunas = comunas[0]
+
+      return Response(comunas)
+
 @api_view(['GET', 'POST'])
 def user_list(req):
     if req.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        
+        user = json.loads(req.body)
+
+        print(user)
+        connection = psycopg2.connect(
+        user='postgres',
+        password="1303",
+        host="localhost",
+        port="5432",
+        database = "sistema_adopcion")
+
+        cursor = connection.cursor()
+        
+
+        cursor.execute(f"""SELECT * FROM sistema_adopcion.usuarios WHERE pass = '{user['pass']}'""")
+        
+
+        userData = cursor.fetchone()
+        userData = userData[0]
+
+
+        jwtpayload = jwt.encode({'payload': userData, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, 'secret', algorithm='HS256')
+
+        print(jwtpayload)
+
+        return Response(userData)
 
     if req.method == 'POST':
-        data = JSONParser().parse(req)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        
+        if req.query_params.get('login') != "":
+
+          user = json.loads(req.body)
+
+          connection = psycopg2.connect(
+          user='postgres',
+          password="1303",
+          host="localhost",
+          port="5432",
+          database = "sistema_adopcion")
+
+          cursor = connection.cursor()
+
+
+          cursor.execute(f"""SELECT ROW_TO_JSON(usuario) FROM (SELECT * FROM sistema_adopcion.usuarios WHERE pass = '{user['pass']}') usuario""")
+
+
+          userData = cursor.fetchone()
+          print(userData)
+
+          if userData != None:
+            
+            userData = userData[0]
+            jwtpayload = jwt.encode({'payload': userData, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)}, 'secret', algorithm='HS256')
+
+            return Response(jwtpayload)
+  
+          else:
+            
+            userData = 'No user matches the provided information'
+
+            return Response(userData)
+
+
+        else:
+
+          user = json.loads(req.body)
+          print(user)
+          connection = psycopg2.connect(
+          user='postgres',
+          password="1303",
+          host="localhost",
+          port="5432",
+          database = "sistema_adopcion")
+
+          cursor = connection.cursor()
+
+          # print(f"""INSERT INTO sistema_adopcion.usuarios (rut, nombre, email, direccion, comuna_id, tipo_usuario_id, fecha_creacion, pass)
+          #                  VALUES ('{user['rut']}', '{user['nombre']}', '{user['email']}', '{user['direccion']}', {user['comuna_id']}, 1, CURRENT_TIMESTAMP, '{user['password']}') RETURNING id;""")
+          cursor.execute(f"""INSERT INTO sistema_adopcion.usuarios (rut, nombre, email, direccion, comuna_id, tipo_usuario_id, fecha_creacion, pass, telefono)
+                           VALUES ('{user['rut']}', '{user['nombre']}', '{user['email']}', '{user['direccion']}', {user['comuna_id']}, 1, CURRENT_TIMESTAMP, '{user['password']}', '{user['telefono']}') RETURNING id;""")
+
+          userId = cursor.fetchone()
+          userId = userId[0]
+
+          connection.commit()
+
+          cursor.execute(f"""SELECT * FROM sistema_adopcion.f_obtener_usuario_por_id({userId})""")
+
+          userData = cursor.fetchone()
+          userData = userData[0]
+
+          return Response(userData)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_detail(req, pk):   
@@ -280,4 +387,38 @@ def user_pets(req, idUser):
 
 
 
+# def load_commerce_data(commerce_code):
+#     print(f"{os.getcwd()}/commerces/597020000541.key")
+#     CERTIFICATES_DIR = os.path.join(os.path.dirname(__file__), "commerces")
+#     print(CERTIFICATES_DIR)
+#     with open(r"C:\Users\timel\Desktop\V\Integracion de Plataformas\integracion-plataformas\sistema_adopcion\sistema_adopcion\commerces\597020000541.key", "r") as file:
+#         key_data = file.read()
+#     with open(r'C:\Users\timel\Desktop\V\Integracion de Plataformas\integracion-plataformas\sistema_adopcion\sistema_adopcion\sistema_adopcion\commerces\597020000541.crt', "r") as file:
+#         cert_data = file.read()
+#     with open(r'C:\Users\timel\Desktop\V\Integracion de Plataformas\integracion-plataformas\sistema_adopcion\sistema_adopcion\sistema_adopcion\commerces\tbk.pem', "r") as file:
+#         tbk_cert_data = file.read()
 
+#     return {
+#         "key_data": key_data,
+#         "cert_data": cert_data,
+#         "tbk_cert_data": tbk_cert_data,
+#     }
+
+
+@api_view(['GET'])
+def donate(req):
+  
+  if req.method == 'GET':
+
+    stripe.api_key = 'sk_test_51H4C4TF9yxhZlW6nhNfbXVZvC7LuYKZx0XRou95dJjgC6cb4WEoHRp7OzI1RJ6MaEqshfAqsnPJivifYYljTKXtC00oKgfX81T'
+
+    intent = stripe.PaymentIntent.create(
+      amount=1099,
+      currency='clp'
+
+      metadata={'integration_check': 'accept_a_payment'}
+    )
+    transaction = []
+
+
+    return Response(transaction)
